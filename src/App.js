932afa010c7026c2ef39629a0f1e5fb5032a5359
Ruthvik Kodati <ruthvik.kodati@students.iiit.ac.in>
2024-11-14 10:50:18 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// // src/App.js
+// import React from 'react';
+// import GraphExplorer from './components/GraphExplorer';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="min-h-screen bg-gray-100 p-8">
+//       <div className="max-w-6xl mx-auto">
+//         <GraphExplorer />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React from "react";
+import NeovisTest from "./NeovisTest";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Testing Neovis.js Connection</h1>
+      <NeovisTest />
     </div>
   );
-}
+};
 
 export default App;
